d0e2fb2df1915e49a5fde43e08e48b8a8358cfe1
Navapache <41594906+Navapache@users.noreply.github.com>
2020-10-06 15:02:02 -0700

    Initial Check-in for Assignment01

---
+++ CSE360/src/cse360Assignment01/AddingMachine.java
@@ -0,0 +1,54 @@
+package cse360Assignment01;
+/**
+ * 
+ * @author Jarren McBide
+ *
+ */
+public class AddingMachine {
+  private int total;
+  /**
+   * Local variable declarations.
+   */
+  public AddingMachine () {
+	  /**
+	   * Initializes the object.
+	   */
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () {
+	  /**
+	   * This method returns the total.
+	   * @return 0
+	   */
+    return 0;
+  }
+  
+  public void add (int value) {
+	  /**
+	   * This method adds the value to the total.
+	   * @param total
+	   */
+  }
+
+  public void subtract (int value) {
+	  /**
+	   * This method subtracts the value from the total.
+	   * @param total
+	   */
+  }
+
+  public String toString () {
+	  /**
+	   * This method returns the history of transactions.
+	   * @return ""
+	   */
+    return "";
+  }
+
+  public void clear() {
+	  /**
+	   * This method clears the object.
+	   */
+  }
+}
